0f3f3096ddbe7a74d1fe2f10f77405df3c55f16b
周明明 <zhoumm@yomob.com>
2019-01-07 17:00:53 +0800

    1. 测试

--- src/main/java/com.design.pattern/interceptingPattern/InterceptingFilterMain.java
+++ src/main/java/com.design.pattern/interceptingPattern/InterceptingFilterMain.java
@@ -8,6 +8,8 @@ public class InterceptingFilterMain {
 
     public static void main(String[] args) {
         FilterManager filterManager = new FilterManager(new Target());
+
+
         filterManager.setFilter(new AuthenticationFilter());
         filterManager.setFilter(new DebugFilter());
 
